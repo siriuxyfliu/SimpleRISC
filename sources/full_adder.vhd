49a7482197c686b7cfc0250e7c37a8132546e850
YifeiLiu <yfliucmu@gmail.com>
2018-02-22 18:26:01 -0500

    full_adder

---
+++ sources/full_adder.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 22.02.2018 18:22:10
+-- Design Name: 
+-- Module Name: full_adder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity full_adder is
+    Port ( a0 : in STD_LOGIC;
+           a1 : in STD_LOGIC;
+           c_in : in STD_LOGIC;
+           c_out : in STD_LOGIC);
+end full_adder;
+
+architecture Behavioral of full_adder is
+
+begin
+
+
+end Behavioral;
