a2841f1110be8d26ef3e712bd9217078523e64e3
YifeiLiu <yfliucmu@gmail.com>
2018-02-22 17:56:49 -0500

    AdderDesign
    Should start from 4 bit.

--- sources/adder.vhd
+++ sources/adder.vhd
@@ -33,20 +33,33 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity adder is
     Port ( clk_i : in STD_LOGIC;
+           rst : in std_logic;
            a1 : in STD_LOGIC_VECTOR (31 downto 0);
            a2 : in STD_LOGIC_VECTOR (31 downto 0);
            s1 : out STD_LOGIC_VECTOR (31 downto 0));
 end adder;
 
 architecture Behavioral of adder is
-   
+   signal step : std_logic_vector (3 downto 0); 
+   signal carry : std_logic_vector (31 downto 0); 
 begin
     process (clk_i)
     begin
         if rising_edge(clk_i) then
-           -- s1(0) <= a1(0) xor a1(0); 
-            --s1(1) <= a1(0) and a2(0); 
-            s1<=(a1(0) and a2(0),a1(0) xor a2(0),others=>'0');
+            if rst='1' then
+                step <= "0000";
+                s1<=(others=>'0');     
+                carry<=(others=>'0');
+            else
+                if step="0000" then
+                    carry<=
+                else
+                -- s1(0) <= a1(0) xor a1(0); 
+                --s1(1) <= a1(0) and a2(0); 
+                s1<=(a1(0) and a2(0),a1(0) xor a2(0),others=>'0');
+            
+            
+            end if;
         end if;
     end process;
 
